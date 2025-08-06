57ce380b7ce3b27735003e26d0f89c1ef1b95872
Maruf <marufsherikar32@gmail.com>
2025-08-06 17:12:06 +0530

    String function practiced

---
+++ src/String/create_fun.java
@@ -0,0 +1,23 @@
+package src.String;
+
+public class create_fun {
+
+    public static void main(String[] args) {
+
+
+        int d = sum(4,5);
+        System.out.println(d);
+    }
+
+
+
+
+
+    public static int sum(int a, int b){
+        return a + b;
+    }
+
+
+
+
+}
